ebcb0523949ad875f1d824634bd587436b69bbab
GOso56 <gauthier.fidry@phelma.grenoble-inp.fr>
2017-10-22 17:44:11 +0200

    Creation du fichier jobs.c. A l'heure actuelle, ca ne compile, <dereferencing pointeur to incomplete typre List jobs.c:15:13>

---
+++ src/jobs.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "jobs.h"
+
+#define CMD_MAX 200
+
+void free_elem(List *p_liste) ;
+void free_elem(List *p_liste) ;
+
+List *create_liste(Job *job) {
+        List *list = malloc(sizeof(list));
+        if (list && job) {
+        list->previous = NULL;
+        list->next = NULL;
+        list->jobs = job;
+        }
+return list;   
+}
+
+List *list_prepend(List *old, Job *jobs){
+List *list = create_liste(jobs);
+if (list){
+        list->next = old;
+        list->previous = NULL;
+        old->previous = list;
+}
+return list;
+}
+
+void del_jobs(List **p_liste) {
+if (! (*p_liste)->previous) { //premier elem de la liste
+        List *list_return = (*p_liste)->next;
+        free_elem(*p_liste);
+        *p_liste = list_return ;
+}
+else {
+        (*p_liste)->previous->next = (*p_liste)->next ;
+        if ((*p_liste)->next)
+                (*p_liste)->next->previous = (*p_liste)->previous;
+        free_elem(*p_liste);
+}
+}
+
+void create_job(pid_t pid, char *cmd, int id, List **jobs) {
+        Job *j = malloc(sizeof(Job));
+        j->pid = pid;
+        j->id = id;
+        strncpy(j->cmd, cmd, CMD_MAX);
+        *jobs = list_prepend(*jobs, j); 
+}
+
+void free_list(List *liste) {
+    for(List *cur = liste; cur != NULL; ){
+        List *next = cur->next;
+        free_elem(cur);
+        cur=next; //HYPER IMPORTANT !! PAS CUR->NEXT!!!!!
+                  // Lancer Valgrind ./listecgainee
+    }
+}
+
+void free_elem(List *p_liste) {
+        free(p_liste->jobs->cmd);
+        free(p_liste->jobs);
+        free(p_liste);
+}
