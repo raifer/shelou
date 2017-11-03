229a284c24a18c1188dbcf14b2e67ab2257f06d0
Mathieu Barbe <mat.barbe@gmail.com>
2017-11-03 11:06:36 +0100

    Amélioration de la gestion de l'affichage quand on attend la fin d'un fils dans execute

--- src/execute.c
+++ src/execute.c
@@ -179,16 +179,14 @@ int execute(char **seq, int in, int out, int bg) {
 		if (pidEnd == -1) {
 			perror("Wait error :");
 			return EXIT_FAILURE;
-		} else {
-			//if (WIFEXITED(status)) printf("Le fils %d c'est terminé avec success avec comme code de retour %d\n", pidEnd, WEXITSTATUS(status));
-			//else printf("Le fils %d a rencontrer une erreur\n", pidEnd);
-			if (!WIFEXITED(status)) printf("Le fils %d a rencontrer une erreur\n", pidEnd);
-
-			if (WIFSIGNALED(status)) printf("Le fils %d c'est terminé à cause du signal n° %d : %s\n", pidEnd, WTERMSIG(status), strsignal(WTERMSIG(status)));
-			// printf("Wait à  terminé avec la fin du fils %d\n", pid_end);
-			return EXIT_SUCCESS;
 		}
-	}
-}
+		if (WIFEXITED(status)) {
+				//printf("Le fils %d c'est terminé avec success avec comme code de retour %d\n", pidEnd, WEXITSTATUS(status));
+			} else {
+				if (WIFSIGNALED(status)) printf("Le fils %d c'est terminé avec une erreur, signal reçu n° %d : %s\n", pidEnd, WTERMSIG(status), strsignal(WTERMSIG(status)));
+			}
+			return EXIT_SUCCESS;
+	} // end switch
+} // end execute
 
 
